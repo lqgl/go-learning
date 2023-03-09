79d706742163efb592b5c4efce7fb38a1ab08f1b
kami <liuguang@duck.com>
2023-03-10 00:19:38 +0800

    refactor: 01-hello-world

---
+++ tdd-leaning/01-hello-world/v8/hello.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const spanish = "Spanish"
+const french = "French"
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
