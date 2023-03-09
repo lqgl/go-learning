ab52e45dd1119b71bdbed103747cabafc91b969d
kami <liuguang@duck.com>
2023-03-09 23:46:28 +0800

    refacto: 01-hello-world

---
+++ tdd-leaning/01-hello-world/v4/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+	return englishHelloPrefix + name
+}
+
+func main() {
+	fmt.Println(Hello("world"))
+}
