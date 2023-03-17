b5e65b40aa8ab061daba48a2908d284f2bb322f8
kami <liuguang@duck.com>
2023-03-17 21:23:55 +0800

    ✨ feat: 11-select

---
+++ tdd-learning/11-select/v2/racer.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) string {
+	startA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
+
+	if aDuration < bDuration {
+		return a
+	}
+
+	return b
+}
